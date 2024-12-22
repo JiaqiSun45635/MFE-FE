948fffba0a3809b53b9f7b6a78e2ba473deb06c3
JiaqiSun45635 <146023836+JiaqiSun45635@users.noreply.github.com>
2024-12-22 11:24:50 +0000

    commit

---
+++ MFE_FE_PW2_Code.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+def first_function(a, b):
+# Do something
+#return "something"
+
+def used_by_second_function(x, y):
+# Do something
+#return "something", "else"
+
+def second_function(x, y, z):
+#w = used_by_second_function(x, y)
+#return w[0], w[1], z
